4ed8036c525a4f5dc875295e7eda3ce8fbced585
Gorky Palacios Mutis <gorky.palacios@paymentevolution.com>
2025-01-31 02:54:42 -0500

    Update payment service endpoint URL for consistency

--- services/paymentService.js
+++ services/paymentService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const pagarConTarjeta = async (paymentDetails) => {
   return await axios.post(
-    "https://api-fake-paymentportal-production.up.railway.app/v1/payment/card",
+    "https://api-fake-paymentportal-production.up.railway.app/api/v1/payment/card",
     paymentDetails
   );
 };
