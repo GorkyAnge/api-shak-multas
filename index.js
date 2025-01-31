1271835f15c82e703adc16a145ed0fa4b75e87ab
Gorky Palacios Mutis <gorky.palacios@paymentevolution.com>
2025-01-31 02:34:40 -0500

    Add CORS support and update dependencies in the Express app

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multasRoutes = require('./routes/multas');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Agrega cors
+const multasRoutes = require("./routes/multas");
 
 const app = express();
+
+// Habilita CORS para cualquier origen
+app.use(cors()); // Esto permite solicitudes desde cualquier origen
+
 app.use(bodyParser.json());
 
-app.use('/multas', multasRoutes);
+app.use("/multas", multasRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
