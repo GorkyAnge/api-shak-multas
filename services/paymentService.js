435b3843d584735d25bb0c9d16c2c899280bb530
GorkyAnge <92492732+GorkyAnge@users.noreply.github.com>
2025-06-14 18:28:37 -0500

    Update paymentService.js

--- services/paymentService.js
+++ services/paymentService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const pagarConTarjeta = async (paymentDetails) => {
   return await axios.post(
-    "https://api-fake-paymentportal-production.up.railway.app/api/v1/payment/card",
+    "https://api-fake-paymentportal-cbea796d6f87.herokuapp.com/api/v1/payment/card",
     paymentDetails
   );
 };
